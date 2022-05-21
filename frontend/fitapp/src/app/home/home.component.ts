d94f640d009094e481e91a91771345b4ed1c99fb
aloralove <aloralove89@gmail.com>
2022-05-21 11:03:35 -0800

    "added top challenges and recipes to home page"

--- frontend/fitapp/src/app/home/home.component.ts
+++ frontend/fitapp/src/app/home/home.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Workouts } from '../workouts';
 import { WorkoutService } from '../workouts.service';
+import { ChallengesList } from '../challengesList';
+import { ChallengesListService } from '../challengesList.service';
+import { NutritionList } from '../nutritionList';
+import { NutritionListService } from '../nutritionList.service';
+
 
 @Component({
   selector: 'app-home',
@@ -11,16 +16,34 @@ import { WorkoutService } from '../workouts.service';
 export class HomeComponent implements OnInit {
 
   workout: Workouts[] = [];
+  challenges: ChallengesList[] = [];
+  nutrition: NutritionList[] = [];
 
-  constructor(private workoutService: WorkoutService) { }
+  constructor(
+    private workoutService: WorkoutService,
+    private challengesListService: ChallengesListService,
+    private nurtitionListService: NutritionListService,
+    ) { }
 
   ngOnInit(): void {
     this.getWorkout();
+    this.getChallenges();
+    this.getNutrition();
   }
 
   getWorkout(): void {
     this.workoutService.getWorkout()
       .subscribe(workout => this.workout = workout.slice(1, 5));
+  }
+
+  getChallenges(): void {
+    this.challengesListService.getChallenges()
+        .subscribe(challenges => this.challenges = challenges.slice(1, 5));
+  }
+
+  getNutrition(): void {
+    this.nurtitionListService.getNutrition()
+        .subscribe(nutrition => this.nutrition = nutrition.slice(1, 5));
   }
 
 }
--- frontend/fitapp/src/app/nutrition/nutrition.component.ts
+++ frontend/fitapp/src/app/nutrition/nutrition.component.ts
@@ -14,8 +14,6 @@ export class NutritionComponent implements OnInit {
 
   constructor(
     private nurtitionListService: NutritionListService,
-
-    
     ) { }
 
   ngOnInit(): void {
